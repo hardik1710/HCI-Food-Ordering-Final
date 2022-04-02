88e1a246249bc267f54f6c0f40b182ef71809bce
hardik1710 <sachdeva.hardik@gmail.com>
2022-04-02 11:16:20 -0400

    Update delete_orders.php
    Updated Item Deletion Query based on Random_id

--- delete_orders.php
+++ delete_orders.php
@@ -5,7 +5,7 @@ session_start();
 
 
 // sending query
-mysqli_query($db,"DELETE FROM users_orders WHERE o_id = '".$_GET['order_del']."'"); // deletig records on the bases of ID
+mysqli_query($db,"DELETE FROM users_orders WHERE random_id = '".$_GET['order_del']."'"); // deletig records on the bases of ID
 header("location:your_orders.php");  //once deleted success redireted back to current page
 
 ?>
